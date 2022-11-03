4e240ff1e33ea32973b17bc0d294297d8fd82bf5
David Radunz <david@vimtura.com>
2022-11-02 22:16:08 -0300

    Added self signed generation stuff.

--- src/certs/csr.rs
+++ src/certs/csr.rs
@@ -1,11 +1,13 @@
+use openssl::asn1::Asn1Time;
+use openssl::bn::{BigNum, MsbOption};
 use openssl::error::ErrorStack;
 use openssl::hash::MessageDigest;
 use openssl::nid::Nid;
 use openssl::pkey::{PKey, Private};
 use openssl::rsa::{Rsa};
 use openssl::stack::Stack;
-use openssl::x509::{X509Name, X509Req};
-use openssl::x509::extension::{KeyUsage, SubjectAlternativeName};
+use openssl::x509::{X509, X509Name, X509Ref, X509Req};
+use openssl::x509::extension::{AuthorityKeyIdentifier, BasicConstraints, KeyUsage, SubjectAlternativeName, SubjectKeyIdentifier};
 
 pub fn generate_rsa_2048_priv_key() -> Result<PKey<Private>, ErrorStack> {
     PKey::from_rsa(Rsa::generate(2048)?)
@@ -18,6 +20,7 @@ pub struct Csr {
     country: Option<String>,
     org_name: Option<String>,
     org_unit: Option<String>,
+    description: Option<String>,
 }
 
 impl Csr {
@@ -29,6 +32,7 @@ impl Csr {
             country: None,
             org_name: None,
             org_unit: None,
+            description: None,
         }
     }
 
@@ -57,6 +61,11 @@ impl Csr {
 
     pub fn with_org_unit(&mut self, org_unit: String) -> &mut Self {
         self.org_unit = Some(org_unit);
+        self
+    }
+
+    pub fn with_description(&mut self, description: String) -> &mut Self {
+        self.description = Some(description);
         self
     }
 
@@ -100,12 +109,33 @@ impl Csr {
     pub fn org_unit(&self) -> Option<String> {
         self.org_unit.clone()
     }
+
+    pub fn description(&self) -> Option<String> {
+        self.description.clone()
+    }
+
+    // Util
+    pub fn subject_alt_names(&self) -> Vec<SubjectAlternativeName> {
+        let mut res: Vec<SubjectAlternativeName> = Vec::new();
+
+        if let Some(alt_names) = self.alt_names.as_ref() {
+            for alt in alt_names {
+                let mut subject_alt_name = SubjectAlternativeName::new();
+                if self.alt_name_is_ip {
+                    subject_alt_name.ip(alt);
+                } else {
+                    subject_alt_name.dns(alt);
+                }
+
+                res.push(subject_alt_name);
+            }
+        }
+
+        res
+    }
 }
 
-pub fn generate_csr(
-    pkey: &PKey<Private>,
-    csr: &Csr
-) -> Result<X509Req, ErrorStack>{
+pub fn extract_name_from_csr(csr: &Csr) -> Result<X509Name, ErrorStack> {
     let mut name = X509Name::builder()?;
     name.append_entry_by_nid(Nid::COMMONNAME, csr.common_name.as_str())?;
 
@@ -118,11 +148,21 @@ pub fn generate_csr(
     if let Some(org_unit) = csr.org_unit.as_ref() {
         name.append_entry_by_nid(Nid::ORGANIZATIONALUNITNAME, org_unit.as_str())?;
     }
+    if let Some(description) = csr.description.as_ref() {
+        name.append_entry_by_nid(Nid::DESCRIPTION, description.as_str())?;
+    }
 
-    let name = name.build();
+    Ok(name.build())
+}
+
+pub fn generate_csr(
+    pkey: &PKey<Private>,
+    csr: &Csr
+) -> Result<X509Req, ErrorStack>{
+    let mut name = extract_name_from_csr(csr)?;
 
     let mut builder = X509Req::builder()?;
-    builder.set_version(0)?;
+    builder.set_version(2)?;
     builder.set_subject_name(&name)?;
     builder.set_pubkey(&pkey)?;
 
@@ -133,21 +173,11 @@ pub fn generate_csr(
         .build()?;
     extensions.push(key_usage).unwrap();
 
-    if let Some(alt_names) = csr.alt_names.as_ref() {
-        for alt in alt_names {
-            let mut subject_alternative_name = SubjectAlternativeName::new();
-            if csr.alt_name_is_ip {
-                subject_alternative_name.ip(alt);
-            } else {
-                subject_alternative_name.dns(alt);
-            }
-
-            let subject_alternative_name = subject_alternative_name
-                .build(&builder.x509v3_context(None))?;
-
-            extensions.push(subject_alternative_name)?;
-        }
+    for subject_alt_name in csr.subject_alt_names() {
+        extensions.push(subject_alt_name
+            .build(&builder.x509v3_context(None))?)?;
     }
+
     builder.add_extensions(&extensions)?;
 
     builder.sign(&pkey, MessageDigest::sha256())?;
@@ -155,6 +185,101 @@ pub fn generate_csr(
     Ok(builder.build())
 }
 
+pub fn generate_ca(
+    pkey: &PKey<Private>,
+    csr: &Csr,
+    days: Option<u32>
+) -> Result<X509, ErrorStack>{
+    let mut name = extract_name_from_csr(csr)?;
+
+    let mut builder = X509::builder()?;
+    builder.set_version(2)?;
+    let serial_number = {
+        let mut serial = BigNum::new()?;
+        serial.rand(159, MsbOption::MAYBE_ZERO, false)?;
+        serial.to_asn1_integer()?
+    };
+    builder.set_serial_number(&serial_number)?;
+    builder.set_subject_name(&name)?;
+    builder.set_issuer_name(&name)?;
+    builder.set_pubkey(&pkey)?;
+    let not_before = Asn1Time::days_from_now(0)?;
+    builder.set_not_before(&not_before)?;
+    let not_after = Asn1Time::days_from_now(days.unwrap_or(365))?;
+    builder.set_not_after(&not_after)?;
+
+    builder.append_extension(BasicConstraints::new().critical().ca().build()?)?;
+    builder.append_extension(
+        KeyUsage::new()
+            .critical()
+            .key_cert_sign()
+            .crl_sign()
+            .build()?,
+    )?;
+
+    let subject_key_identifier =
+        SubjectKeyIdentifier::new().build(&builder.x509v3_context(None, None))?;
+    builder.append_extension(subject_key_identifier)?;
+
+    builder.sign(&pkey, MessageDigest::sha256())?;
+
+    Ok(builder.build())
+}
+
+pub fn generate_ca_signed_cert(
+    pkey: &PKey<Private>,
+    csr: &Csr,
+    ca: &X509Ref,
+    days: Option<u32>
+) -> Result<X509, ErrorStack>{
+    let req = generate_csr(pkey, csr)?;
+
+    let mut builder = X509::builder()?;
+    builder.set_version(2)?;
+    let serial_number = {
+        let mut serial = BigNum::new()?;
+        serial.rand(159, MsbOption::MAYBE_ZERO, false)?;
+        serial.to_asn1_integer()?
+    };
+    builder.set_serial_number(&serial_number)?;
+    builder.set_subject_name(req.subject_name())?;
+    builder.set_issuer_name(ca.subject_name())?;
+    builder.set_pubkey(&pkey)?;
+    let not_before = Asn1Time::days_from_now(0)?;
+    builder.set_not_before(&not_before)?;
+    let not_after = Asn1Time::days_from_now(days.unwrap_or(365))?;
+    builder.set_not_after(&not_after)?;
+
+    builder.append_extension(BasicConstraints::new().build()?)?;
+
+    builder.append_extension(
+        KeyUsage::new()
+            .critical()
+            .non_repudiation()
+            .digital_signature()
+            .key_encipherment()
+            .build()?,
+    )?;
+
+    let subject_key_identifier =
+        SubjectKeyIdentifier::new().build(&builder.x509v3_context(Some(ca), None))?;
+    builder.append_extension(subject_key_identifier)?;
+
+    let auth_key_identifier = AuthorityKeyIdentifier::new()
+        .keyid(false)
+        .issuer(false)
+        .build(&builder.x509v3_context(Some(ca), None))?;
+    builder.append_extension(auth_key_identifier)?;
+
+    for subject_alt_name in csr.subject_alt_names() {
+        builder.append_extension(subject_alt_name
+            .build(&builder.x509v3_context(Some(ca), None))?)?;
+    }
+
+    builder.sign(pkey, MessageDigest::sha256())?;
+
+    Ok(builder.build())
+}
 
 #[cfg(test)]
 mod tests {
