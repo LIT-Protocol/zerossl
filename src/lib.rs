af9040dd0ae5c676df499f42e69d762eb95f5817
David Radunz <david@vimtura.com>
2022-11-04 22:22:09 -0300

    Trying something

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,8 @@ pub mod error;
 pub mod client;
 pub mod certs;
 
-#[allow(unused_imports)]
-use client::Client;
-#[allow(unused_imports)]
-use client::certificates::{CreateCertificateReq, CreateCertificateRes, ListCertificatesReq, ListCertificatesRes, VerifyCertificateRes, VerifyCertificateReq, DownloadCertificateRes};
-#[allow(unused_imports)]
-use error::Result;
+pub use error::{Result, Error};
+pub use certs::csr::{generate_csr, generate_ca, generate_ca_signed_cert, generate_rsa_2048_priv_key};
+pub use client::Client;
+pub use client::certificates::{CreateCertificateReq, CreateCertificateRes, ListCertificatesReq, ListCertificatesRes, VerifyCertificateRes, VerifyCertificateReq, DownloadCertificateRes};
+pub use client::result::{ResultStatus, ResultStatusAlt, ErrorMsg, Resp};
