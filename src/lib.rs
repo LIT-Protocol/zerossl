e30091a74b3b40f53b9f6e12915ef3c72d9a93bd
David Radunz <david@vimtura.com>
2022-11-04 22:18:49 -0300

    Trying something

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
 pub mod error;
 pub mod client;
 pub mod certs;
+
+#[allow(unused_imports)]
+use client::Client;
+#[allow(unused_imports)]
+use client::certificates::{CreateCertificateReq, CreateCertificateRes, ListCertificatesReq, ListCertificatesRes, VerifyCertificateRes, VerifyCertificateReq, DownloadCertificateRes};
+#[allow(unused_imports)]
+use error::Result;
